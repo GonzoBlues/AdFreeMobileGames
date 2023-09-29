b89cb9c957a45de51e9b429432bef297232a6685
GonzoBlues <Ian@frogsfolly.com>
2023-09-29 17:42:54 -0500

    added some parts to start making the move color

--- WaterSortV1.py
+++ WaterSortV1.py
@@ -70,17 +70,68 @@ def FillTubes(Height, ColorList, NestedTubes):
     for i in range(len(colors_to_assign)):
         tube = NestedTubes[i % num_tubes]
         tube.append(colors_to_assign.pop(0))
+
+def findLastNonEmptyLine(tube):
+    for i in range(len(tube) - 1, 0, -1):
+        if i == 0:
+            return 0
+        if tube[i] != "{}":
+            return i
+    return -1  # Return -1 if the tube is completely empty
+
+def FindColorInARow(tube, tubeContains):
+    matching_count = 0
+    Endpoint = 0
+    for i in range(len(tube) - 1, 0, -1):
+        if tube[i] == tubeContains:
+            Endpoint = i
+    end_color = tube[Endpoint]
+    for i in range(Height, 0, -1):
+        if tube[i] == end_color:
+            matching_count += 1
+        else:
+            break  # Stop counting when a different color is encountered
+    return matching_count
+
+def moveColor(tube1, tube2, Height):
+    last_non_empty_line_tube1 = findLastNonEmptyLine(tube1)
+    last_non_empty_line_tube2 = findLastNonEmptyLine(tube2)
+    if last_non_empty_line_tube1 == 0:
+        print("You can't select an empty Tube First")
+        
+    if last_non_empty_line_tube1 == 0:
+
+    if last_non_empty_line_tube1 != -1 and last_non_empty_line_tube2 != -1:
+        if tube1[last_non_empty_line_tube1] == tube2[last_non_empty_line_tube2]:
+            print(f"Last non-empty item in tube1: {tube1[last_non_empty_line_tube1]}")
+            print(f"Last non-empty item in tube2: {tube2[last_non_empty_line_tube2]}")
+        
+    if tube1[Height] == tube2[Height]:        
+        # Check how many elements from the end match the end color
+        matching_count = 0
+        end_color = tube1[Height]  # Assuming the end color is at the last index (Height)
+        for i in range(Height, 0, -1):
+            if tube1[i] == end_color:
+                matching_count += 1
+            else:
+                break  # Stop counting when a different color is encountered
+        
+        print(f"Matching elements from the end: {matching_count}")
     
-    for tube in NestedTubes:
-        print(tube)
-
-
-
 
 TubeNumber = 4
-Height = 4
+Height = 4 
 CreateTubes(Height,TubeNumber)
 
 selected_colors = PickColors(color_dict, TubeNumber)
 print("Selected Colors:", selected_colors)
 FillTubes(Height, selected_colors, NestedTubes)
+
+for tube in NestedTubes:
+        print(tube)
+
+TestList1 = ["Tube1","Blue","Blue","Green","Blue","Blue","Blue","Blue","Blue","Blue"]
+TestList2 = ["Tube2","Blue","Blue","Blue","Blue","Blue","Blue","Blue","Blue","Blue"]
+
+moveColor(NestedTubes[1],NestedTubes[1],Height)
+moveColor(TestList1,TestList2,10)
